60e2265a7472a1bab7fa9f09c01ca6385537e17e
YuriSib <sib_94@vk.com>
2026-01-05 12:05:06 +0300

    2

--- scrapper.py
+++ scrapper.py
@@ -51,14 +51,15 @@ async def async_fetch_data(url, proxy, query, page):
     cookies = {
         '_wbauid': '2451425061762531645',
         '_cp': '1',
-        'x_wbaas_token': '1.1000.40fb3ff31a7e4d41839c61bf50715be7.MHw5NC4yMzAuMzUuNTN8TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvMTM4LjAuMC4wIFlhQnJvd3Nlci8yNS44LjAuMCBTYWZhcmkvNTM3LjM2fDE3NjUwMjMzMDN8cmV1c2FibGV8MnxleUpvWVhOb0lqb2lJbjA9fDB8M3wxNzY0NDE4NTAzfDE=.MEYCIQC03F/Vrly5R58N/JVmPp8kuYcQWkYLJ8ewbQ5O4Hj7TwIhAMEk6fZyJ65oq2U9t4hQl+QeXrs5wp9mRF05Ivu1FGN6',
+        'x_wbaas_token': '1.1000.562e9ba4c5b245c499850f3f214a7af5.MHw5NC4yMzAuMzUuNTN8TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvMTM4LjAuMC4wIFlhQnJvd3Nlci8yNS44LjAuMCBTYWZhcmkvNTM3LjM2fDE3Njc1OTc0NzB8cmV1c2FibGV8MnxleUpvWVhOb0lqb2lJbjA9fDB8M3wxNzY2OTkyNjcwfDE=.MEQCIAoDD7RRKvOwUeIhvY+GRjTjxeLzq5ZCsIuqY03HtZ5JAiAB2vrF8ppEeCbf1w07EpyRqHyBH83ahNqc0KE4c2AdSg==',
     }
+
     headers = {
         'accept': '*/*',
         'accept-language': 'ru,en;q=0.9',
         'deviceid': 'site_bf631b5e4bff4512995738aa60ca1914',
         'priority': 'u=1, i',
-        'referer': 'https://www.wildberries.ru/catalog/0/search.aspx?search=%D0%A0%D0%A3%D0%B1%D0%B0%D1%88%D0%BA%D0%B0',
+        'referer': 'https://www.wildberries.ru/catalog/0/search.aspx?search=%D0%BA%D1%80%D0%BE%D1%81%D1%81%D0%BE%D0%B2%D0%BA%D0%B8%20%D0%BC%D1%83%D0%B6%D1%81%D0%BA%D0%B8%D0%B5%20%D0%B7%D0%B8%D0%BC%D0%BD%D0%B8%D0%B5',
         'sec-ch-ua': '"Not)A;Brand";v="8", "Chromium";v="138", "YaBrowser";v="25.8", "Yowser";v="2.5"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Linux"',
@@ -66,11 +67,11 @@ async def async_fetch_data(url, proxy, query, page):
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 YaBrowser/25.8.0.0 Safari/537.36',
-        'x-queryid': 'qid245142506176253164520251122160607',
+        'x-queryid': 'qid245142506176253164520251222071923',
         'x-requested-with': 'XMLHttpRequest',
-        'x-spa-version': '13.14.1',
+        'x-spa-version': '13.17.4',
         'x-userid': '0',
-        # 'cookie': '_wbauid=2451425061762531645; _cp=1; x_wbaas_token=1.1000.40fb3ff31a7e4d41839c61bf50715be7.MHw5NC4yMzAuMzUuNTN8TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvMTM4LjAuMC4wIFlhQnJvd3Nlci8yNS44LjAuMCBTYWZhcmkvNTM3LjM2fDE3NjUwMjMzMDN8cmV1c2FibGV8MnxleUpvWVhOb0lqb2lJbjA9fDB8M3wxNzY0NDE4NTAzfDE=.MEYCIQC03F/Vrly5R58N/JVmPp8kuYcQWkYLJ8ewbQ5O4Hj7TwIhAMEk6fZyJ65oq2U9t4hQl+QeXrs5wp9mRF05Ivu1FGN6',
+        # 'cookie': '_wbauid=2451425061762531645; _cp=1; x_wbaas_token=1.1000.562e9ba4c5b245c499850f3f214a7af5.MHw5NC4yMzAuMzUuNTN8TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvMTM4LjAuMC4wIFlhQnJvd3Nlci8yNS44LjAuMCBTYWZhcmkvNTM3LjM2fDE3Njc1OTc0NzB8cmV1c2FibGV8MnxleUpvWVhOb0lqb2lJbjA9fDB8M3wxNzY2OTkyNjcwfDE=.MEQCIAoDD7RRKvOwUeIhvY+GRjTjxeLzq5ZCsIuqY03HtZ5JAiAB2vrF8ppEeCbf1w07EpyRqHyBH83ahNqc0KE4c2AdSg==',
     }
     params = {
         'ab_testing': [
